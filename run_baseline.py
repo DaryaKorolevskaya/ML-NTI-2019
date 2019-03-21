f3082c05d6528a81676082579e4395e01222a562
DaryaKorolevskaya <31251742+DaryaKorolevskaya@users.noreply.github.com>
2019-03-21 15:56:15 +0300

    Update run_baseline.py

--- run_baseline.py
+++ run_baseline.py
@@ -31,7 +31,7 @@ def train(num_timesteps, seed, model_path=None, env_id=None):
                              clip_param=0.2, entcoeff=0.0,
                              optim_epochs=10,
                              optim_stepsize=3e-4,
-                             optim_batchsize=164,
+                             optim_batchsize=64,
                              gamma=0.99,
                              lam=0.95,
                              schedule='linear',
